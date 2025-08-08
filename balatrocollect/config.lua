d30b6388230dd84974446190ee17636a1bb3fdc3
Aidan-Jared <AidanJared42@gmail.com>
2025-08-09 07:21:41 +1200

    adding code from besteon/balatrobot to start making data collection API

---
+++ balatrocollect/config.lua
@@ -0,0 +1,15 @@
+BALATRO_BOT_CONFIG = {
+    enabled = true, -- Disables ALL mod functionality if false
+    port = '12345', -- Port for the bot to listen on, overwritten by arg[1]
+    dt = nil, -- Tells the game that every update is dt seconds long
+    uncap_fps = false,
+    instant_move = false,
+    disable_vsync = false,
+    disable_card_eval_status_text = false, -- e.g. +10 when scoring a queen
+    frame_ratio = 1, -- Draw every 100th frame, set to 1 for normal rendering
+    
+    passive_mode = true, -- for collecting data while person plays
+    send_all_states = true, -- send data on every state change
+}
+
+return BALATRO_BOT_CONFIG
---
+++ balatrocollect/main.lua
@@ -0,0 +1,38 @@
+--- STEAMODDED HEADER
+--- MOD_NAME: datacollect
+--- MOD_ID: datacollect-v0.1
+--- MOD_AUTHOR: [Aidan-Jared]
+--- MOD_DESCRIPTION: A datacollection API for Balatro
+-- Code taken from besteon/balatrobot and modified
+
+function SMODS.INIT.BALATROBOT()
+	mw = SMODS.findModByID("datacollect-v0.1")
+
+	-- Load the mod configuration
+	assert(load(NFS.read(mw.path .. "config.lua")))()
+	if not BALATRO_BOT_CONFIG.enabled then
+		return
+	end
+
+	-- External libraries
+	assert(load(NFS.read(mw.path .. "lib/list.lua")))()
+	assert(load(NFS.read(mw.path .. "lib/hook.lua")))()
+	assert(load(NFS.read(mw.path .. "lib/bitser.lua")))()
+	assert(load(NFS.read(mw.path .. "lib/sock.lua")))()
+	assert(load(NFS.read(mw.path .. "lib/json.lua")))()
+
+	-- Mod specific files
+	assert(load(NFS.read(mw.path .. "src/utils.lua")))()
+	assert(load(NFS.read(mw.path .. "src/bot.lua")))()
+	assert(load(NFS.read(mw.path .. "src/middleware.lua")))()
+	assert(load(NFS.read(mw.path .. "src/botlogger.lua")))()
+	assert(load(NFS.read(mw.path .. "src/api.lua")))()
+
+	sendDebugMessage("Balatrobot v0.3 loaded")
+
+	Middleware.hookbalatro()
+
+	Botlogger.path = mw.path
+	Botlogger.init()
+	BalatrobotAPI.init()
+end
